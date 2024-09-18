0975e79ee2e7fce4366fe449fead3d870320b017
Kapil Shanbhag - k0s0tts <Kapil.Shanbhag@walmart.com>
2024-09-18 08:41:50 +0530

    Started XCUIElement Query

--- XCTestLearn/ViewController.swift
+++ XCTestLearn/ViewController.swift
@@ -8,14 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private var count: Int = 0
 
+    @IBOutlet weak var testLabel: UILabel!
+    @IBOutlet weak var testButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        testLabel.text = "\(count)"
+        testButton.setTitle("Add", for: .normal)
+        testButton.accessibilityLabel = "MyAdd_Button"
     }
     func isEven(number: Int) -> Bool {
         return number % 2 == 0
     }
 
+    @IBAction func testButtonTapped(_ sender: Any) {
+        count += 1
+        testLabel.text = "\(count)"
+    }
 }
 
